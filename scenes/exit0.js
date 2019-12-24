f5cb1046e240ccc32f05f34b9a848bbc9871449b
Francesco Bedussi <francesco@francescobedussi.it>
2019-12-24 17:55:07 +0100

    fix: exit

--- scenes/exit0.js
+++ scenes/exit0.js
@@ -7,14 +7,14 @@ const keelDefaultActions = require('./keelDefaultActions');
 
 const exit0 = {
     name: 'exit0',
-    shortDesc: `Sei in una minuscola stanzetta, stretta e lunga.`,
-    longDesc: `In alto vedi una luce.`,
+    shortDesc: 'Sei in una minuscola stanzetta, stretta e lunga.',
+    longDesc: 'In alto vedi una luce.',
     actions: {
         ...keelDefaultActions,
-        e: () => `Di qui non si passa, questa è tutta l'aria di essere la chiglia della nave`,
+        e: () => 'Di qui non si passa, questa è tutta l\'aria di essere la chiglia della nave',
         o: () => setCurrentScene('coffers'),
-        u: () => `Provi a fare un salto, ma il boccaporto è troppo alto da raggiungere.`,
-        use_ladder: () => `La scala è troppo corta e inoltre non riesci ad agganciarla al boccaporto`,
+        u: () => 'Provi a fare un salto, ma il boccaporto è troppo alto da raggiungere.',
+        use_ladder: () => 'La scala è troppo corta e inoltre non riesci ad agganciarla al boccaporto',
         leave_coffer: (...args) => {
             incrementPointsBy(10);
             exit0.actions.jump_coffer = () => {
@@ -27,15 +27,15 @@ const exit0 = {
                         setWon();
                         end();
                     };
-                    leave(['leave', 'ladder'], ['lascia', 'scala di corda']);
-    
-                    return `Riesci a lanciare la scala abbastanza in alto da agganciarla ai ganci. Puoi salire!`
+                    leave({ rawInput: ['leave', 'ladder'], parsedInput: ['lascia', 'scala di corda'] });
+
+                    return 'Riesci a lanciare la scala abbastanza in alto da agganciarla ai ganci. Puoi salire!';
                 };
 
-                return `Adessi sei un po' più in alto, ma ancora non riesci ad arrivare al boccaporto.`
-            }
+                return 'Adessi sei un po\' più in alto, ma ancora non riesci ad arrivare al boccaporto.';
+            };
             return leave(...args);
-        }
+        },
     },
 };
 
